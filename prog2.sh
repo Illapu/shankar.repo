c8473add3e2933e4db13ffdb8c04096086b367d8
Illapu <154864076+Illapu@users.noreply.github.com>
2024-02-11 18:20:06 +0530

    Update prog2.sh

--- prog2.sh
+++ prog2.sh
@@ -4,6 +4,8 @@ echo "the arguments are ${*}"
 echo "the first argument is : $1"
 echo "shifting by one"
 shift 1
+echo "we are working two jobs"
+
 echo "hi there"
 echo "we are working"
 echo "another line"
