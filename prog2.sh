9ab8e8b5192e90abd4a19a96aee53e59a0f0f97c
Illapu <154864076+Illapu@users.noreply.github.com>
2024-02-11 18:40:45 +0530

    Update prog2.sh

--- prog2.sh
+++ prog2.sh
@@ -4,10 +4,6 @@ echo "the arguments are ${*}"
 echo "the first argument is : $1"
 echo "shifting by one"
 shift 1
-echo "we are working two jobs"
-echo "hi there"
-echo "we are working"
-echo "another line"
 echo "no of positional parameters after shift: ${*}"
 echo "the program name is: $0"
 #END 
