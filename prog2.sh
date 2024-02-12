62a51729161dd21ec84f88b9ee4b706725c3e52b
Illapu <154864076+Illapu@users.noreply.github.com>
2024-02-12 17:50:40 +0530

    Update prog2.sh

--- prog2.sh
+++ prog2.sh
@@ -5,6 +5,7 @@ echo "the first argument is : $1"
 echo "we are working arguments"
 echo "shifting by one"
 shift 1
+echo "we are working to hook"
 echo "no of positional parameters after shift: ${*}"
 echo "the program name is: $0"
 #END 
