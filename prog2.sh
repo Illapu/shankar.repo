fcb08f402ce31cc8b51fa2153e878b6f7b58149a
Illapu <154864076+Illapu@users.noreply.github.com>
2024-02-12 14:51:16 +0530

    Update prog2.sh

--- prog2.sh
+++ prog2.sh
@@ -2,6 +2,7 @@
 echo "you are giving the number of aruguments"
 echo "the arguments are ${*}"
 echo "the first argument is : $1"
+echo "we are working arguments"
 echo "shifting by one"
 shift 1
 echo "no of positional parameters after shift: ${*}"
